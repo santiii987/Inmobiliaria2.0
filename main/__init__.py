897b1739aa1884e6e76a3e6cc91edd49f1fe4d9d
santiii987 <cedeira158@gmail.com>
2021-06-09 22:40:47 -0300

    playing with configs 1

--- main/__init__.py
+++ main/__init__.py
@@ -12,11 +12,12 @@ app.secret_key = secret_key
 # app.config['MYSQL_USER'] = 'root'
 # app.config['MYSQL_PASSWORD'] = 'santi987'
 # app.config['MYSQL_DB'] = 'inmobiliaria'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_ECHO'] = False
 #app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ['DATABASE_URL'])
 app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql://manuu307:q1w2e3r$@manuu307.mysql.pythonanywhere-services.com/manuu307$pallaresyasociados' 
 #'sqlite:///database/database_two.db'
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 250
 app.config['WTF_CSRF_ENABLED'] = False
 
 db = SQLAlchemy(app)
